76cf830257fda893dad02ae078a98e6bc4c0e4a4
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-03-25 04:53:30 -0400

    Add Scala Hello World program

---
+++ scala/hello.scala
@@ -0,0 +1,12 @@
+/*
+ * Specifying `main` method for program that doesn't extend `App` trait:
+ *
+ * object HelloWorld {
+ *   def main(arg: Array[String]) = {
+ *     println("Hello World!")
+ *   }
+ * }
+ */
+ object HelloWorld extends App {
+   println("Hello World!")
+ }
