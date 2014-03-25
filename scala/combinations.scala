b8d9ed1beff3b8ab120fb9f790ef0690413dcdc9
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-03-25 06:09:37 -0400

    Add Scala combinations script

---
+++ scala/combinations.scala
@@ -0,0 +1,17 @@
+object Program extends App {
+  /*
+   * For a collection of n items, there is a one-to-one correspondence between
+   * the unique subsets of the collection and the binary representation of the
+   * integer range [0, (2n - 1)], and the total number of subsets is equal to 2n.
+   *
+   * TODO: find a Scala interface that defines some kind of `toArray` method, so
+   * that collections that aren't Arrays can be passed to this method as well.
+   */
+  def combinations(set: Array) = {
+    var subsets = Array()
+    for (i <- 0 to set.size) {
+
+    }
+  }
+}
+
